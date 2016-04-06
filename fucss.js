58e3761c7de4e4f472163048e4e35a8501f33c2e
noneede <noneede@gmail.com>
2016-04-06 01:11:27 +0000

    version 4

--- fucss.js
+++ fucss.js
@@ -1,22 +1,31 @@
 var watch = 5000;
 
 var seps = {
-  'value': '_',
+  'value': ':',
   'space': '-',
-}
+  'target': '_',
+};
 
 var media = {
   mbl: 480,
   tbt: 768,
   lpt: 1024,
   dsk: 1280,
-}
+  
+  sm: 480,
+  md: 768,
+  lg: 1024,
+};
 
 var states = {
   hov: 'hover',
   act: 'active',
   foc: 'focus',
-}
+  
+  hv: 'hover',
+  ac: 'active',
+  fc: 'focus',
+};
 
 var properties = {
   bs: 'box-sizing',
@@ -31,15 +40,10 @@ var properties = {
   h: 'height',
   w: 'width',
   fnt: 'font',
-  flt:  'float',
+  flt: 'float',
   vlgn: 'vertical-align',
   hlgn: 'text-align',
   col: 'width',
-  t: 'top',
-  r: 'right',
-  b: 'bottom',
-  l: 'left',
-  of: 'overflow',
   lh: 'line-height',
   crs: 'cursor',
   dec: 'text-decoration',
@@ -53,9 +57,56 @@ var properties = {
   mxh: 'max-height',
   mnw: 'min-width',
   mnh: 'mib-height',
+  
+  ls: 'list-style',
+  ltrs: 'letter-spacing',
+  ws: 'white-space',
+  
+  //version 4
+  ff: 'font-family',
+  ta: 'text-align',
+  fs: 'font-size',
+  dp: 'display',
+  t: 'top',
+  r: 'right',
+  b: 'bottom',
+  l: 'left',
+  of: 'overflow',
+  br: 'border-radius',
+  bd: 'border',
+  fw: 'font-weight',
+  c: 'color',
+  p: 'padding',
+  m: 'margin',
+  va: 'vertical-align',
+  ps: 'position',
+  xw: 'max-width',
+  nw: 'min-width',
+  td: 'text-decoration',
+  z: 'z-index',
+  
 };
 
 var ignore = ['fa', 'fix', 'trans', 'cursor', 'wrap'];
+
+//version 4
+var colorazable = [
+  'color', 
+  'background', 
+  'background-color', 
+  'border', 
+  'border-color', 
+  'border-bottom',
+  'border-top',,
+  'border-left',
+  'border-right',
+  'border-right-color',
+  'border-left-color',
+  'border-top-color',
+  'border-bottom-color',
+];
+
+var units = ['px', 'em', 'pc', 'vh', 'vw']; 
 
 var groups = ['tb', 'rl'];
 
@@ -77,6 +128,9 @@ var addons = {
   lyt: 'layout',
   wg: 'weight',
   upc: 'uppercase',
+  //version 4
+  c: 'color',
+  s: 'style',
 };
 
 
@@ -91,6 +145,7 @@ var values = {
   t: 'top',
   b: 'bottom',
   m: 'middle',
+  n: 'none',
   clr: 'color',
   rel: 'relative',
   abs: 'absolute',
@@ -104,15 +159,27 @@ var values = {
   hid: 'hidden',
   vis: 'visible',
   pnt: 'pointer',
+  sld: 'solid',
   // added by me
   ib: 'inline-block',
   blk: 'block',
+  
+  //version 4
+  hd: 'hidden',
+  vs: 'visible',
+  bk: 'block',
+  pt: 'pointer',
+  rl: 'relative',
+  ab: 'absolute',
+  sd: 'solid',
+  ts: 'transparent',
+  np: 'nowrap',
 };
 
 
 window.onload=function(){
   generateStyling() && watch && setInterval(generateStyling, watch);
-}
+};
 
 function generateStyling(){
   
@@ -127,29 +194,34 @@ function generateStyling(){
   
   harvestClassesFromOneFile(document.body.outerHTML)
     .forEach(function(className){
-  
-      var splitedClassName = className.split(seps.value);
       
+      var target = className.split(seps.target);
+      
+      var splitedClassName = target.shift().split(seps.value);
+      
+      //props
       var props = splitedClassName.shift().split(seps.space);
       var mediaQuery = extractMediaQuery(props);
       var state = extractState(props);
+      
+      //values
       
       var value = splitedClassName.pop();
       var prop = props.shift();
       
       //console.log(prop, props, state, value);
       
+      
+      if(ignore.indexOf(prop) !== -1){return}
       if(!value){return console.warn('No value specified. Use value seperator ' + seps.value + ' for "' + className + '"')}
       if(!prop){return console.warn('No prop specified. Use prop seperator ' + seps.space + ' for "' + className + '"')}
-      if(ignore.indexOf(prop) !== -1){return}
       if(!properties[prop]){cssMissing = cssMissing.concat([prop])}
       
-      
-      value = modifyValue(value, prop);
-      props = modifyProps(props);
       prop = combineProps(prop, props);
+      props = modifyProps(props);
+      value = modifyValue(value.split(seps.space), prop);
       
-      var cssRule = generateCssRule(className, prop, props, value, state);
+      var cssRule = generateCssRule(className, prop, props, value, state, target);
       
       mediaQuery
         ? cssMediaQueries[mediaQuery] = cssMediaQueries[mediaQuery] ? (cssMediaQueries[mediaQuery] + cssRule) : cssRule
@@ -160,7 +232,7 @@ function generateStyling(){
   //sets media queries at the end
   Object.keys(cssMediaQueries).length 
     && Object.keys(cssMediaQueries).forEach(function(mediaName){
-      cssString += '@media only screen and (min-width: ' + media[mediaName] + 'px) {\n' + cssMediaQueries[mediaName] + '}\n'
+      cssString += '@media only screen and (min-width: ' + media[mediaName] + 'px) {\n' + cssMediaQueries[mediaName] + '}\n';
     });
   
   //console.log(cssString);
@@ -202,21 +274,51 @@ function extractState(props){
   }
 }
 
-function modifyValue(value, prop){
+function modifyValue(valueList, prop){
+  //console.log(prop, value);
+  
+  //console.log(prop, );
+  valueList = valueList.map(function(value){
+    if(colorazable.indexOf(prop) !== -1){
+      
+      //alpha hunter
+      var alpha = 1;
+      var length = value.length;
+      if((length === 5 || length === 8) && (value.lastIndexOf('a') === 3 || value.lastIndexOf('a') === 6)){
+        alpha = value.substring(length-2);
+        value = value.replace(alpha, '');
+        alpha = '0.' + alpha.replace('a', '');
+      }
+      
+      //color hunter
+      if(new RegExp(/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i).test('#' + value)){
+        return hex2rgb(value, alpha);
+      }
+    }
+    
+    if(values[value]){ return values[value] }
+    
+    var unit = value.replace(/\d/g, '');
+    if(unit && (unit.length === 3 || unit.length === 2)){
+      value = value.replace(unit, '');
+      
+      if(unit.indexOf('pc') !== -1 ) return value + '%';
+      if(unit.indexOf('n') !== -1) value = -value;
+      return value + unit.replace('n', '');
+    }
+    return value;
+  });
   
   
-  if(new RegExp(/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i).test('#' + value)){ return '#' + value }
   
-  if(values[value]){ return values[value] }
-  if(value.indexOf('pc') !== -1 ){ return value.replace('pc', '%') }
   
-  return value;
+  return valueList.join(' ');
 }
 
 function modifyProps(props){
   var combinedProps = [];
   props.forEach(function(prop){
-    combinedProps.push(addons[prop] || prop)
+    combinedProps.push(addons[prop] || prop);
   });
   
   return combinedProps;
@@ -228,7 +330,9 @@ function combineProps(prop, props){
   return prop.join('-');
 }
 
-function generateCssRule(className, prop, props, value, state){
+function generateCssRule(className, prop, props, value, state, target){
+  
+  className = className.replace(':', '\\:');
   
   var firstAddon = props.length && props[0];
   var isGroup = groups.indexOf(firstAddon) !== -1;
@@ -236,17 +340,45 @@ function generateCssRule(className, prop, props, value, state){
   state ? className += (':' + state) : false;
   var rules = '';
   
-  if(!isGroup){
-    prop = [prop].concat(props).join('-');
-    rules = prop + ':' + value + ';';
+  //grouped props by comma
+  var groupedProps = prop.split(',');
+  
+  if(groupedProps && groupedProps.length > 1){
+    groupedProps.forEach(function(char){
+      rules += (properties[char] || char) + ':' + value + ';';
+    });
+    className = className.split(',').join('\\,');
+  } else {
+    
+    //grouped addons
+    if(!isGroup){
+      prop = [prop].concat(props).join('-');
+      rules = prop + ':' + value + ';';
+    }else{
+      firstAddon.split('').forEach(function(char){
+        rules += prop + '-' + addons[char] + ':' + value + ';';
+      });
+    }
   }
   
-  if(isGroup){
-    firstAddon.split('').forEach(function(char){
-      rules += prop + '-' + addons[char] + ':' + value + ';';
-    });
+  if(target){
+    var allIndex = target.indexOf('all');
+    if(allIndex !== -1) {target[allIndex] = '*'};
+    className = className + ' ' + target.join(' ');
   }
   
   return '.' + className+ '{' + rules + '}\n';
   
+}
+
+function hex2rgb(hex, opacity) {
+  var h=hex.replace('#', '');
+  h =  h.match(new RegExp('(.{'+h.length/3+'})', 'g'));
+
+  for(var i=0; i<h.length; i++)
+      h[i] = parseInt(h[i].length==1? h[i]+h[i]:h[i], 16);
+
+  if (typeof opacity != 'undefined')  h.push(opacity);
+
+  return 'rgba('+h.join(',')+')';
 }
